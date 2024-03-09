158064bd298df46250fc7be5fa9ce5a39edca29c
Ian Brown <ibrown@netflix.com>
2024-03-09 15:03:10 -0800

    adding more modules

--- MyCompiler.java
+++ MyCompiler.java
@@ -1,3 +1,11 @@
+import jdk.vm.ci.code.CompilationRequest;
+import jdk.vm.ci.code.CompilationRequestResult;
+
 class MyCompiler implements jdk.vm.ci.runtime.JVMCICompiler {
+
+    CompilationRequestResult compileMethod(CompilationRequest request) {
+	System.out.println("COMPILE!!!");
+	return new CompilationRequestResult();
+    }
 
 }
--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-javac --add-modules jdk.internal.vm.ci --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-opens jdk.internal.vm.ci/jdk.vm.ci.meta=ALL-UNNAMED MyCompiler.java 
+javac --add-modules jdk.internal.vm.ci  --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=ALL-UNNAMED --add-opens jdk.internal.vm.ci/jdk.vm.ci.meta=ALL-UNNAMED MyCompiler.java 
