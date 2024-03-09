867aa2f804a418581abdf076a00d7e8f4afd15be
Ian Brown <ibrown@netflix.com>
2024-03-09 14:53:38 -0800

    adding initial project

---
+++ MyCompiler.java
@@ -0,0 +1,3 @@
+class MyCompiler implements jdk.vm.ci.runtime.JVMCICompiler {
+
+}
---
+++ build.sh
@@ -0,0 +1,1 @@
+javac --add-modules jdk.internal.vm.ci --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-opens jdk.internal.vm.ci/jdk.vm.ci.meta=ALL-UNNAMED MyCompiler.java 
