a8eaa055d1733b02959bacdd8aa12ba90c82552d
Ian Brown <ibrown@netflix.com>
2024-08-10 15:52:20 -0700

    sort of kind working? shrug

--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
-java    --upgrade-module-path=mycompiler.jar  --add-modules jdk.internal.vm.ci --add-modules jdk.internal.vm.compiler --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=ALL-UNNAMED  -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation  -XX:+LogVMOutput  -XX:-TieredCompilation    -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI  -XX:+UseJVMCICompiler -Djvmci.Compiler=org.hccp.compiler.MyCompilerFactory  -XX:+PrintCompilation -jar mycompiler.jar 
+#java    --upgrade-module-path=mycompiler.jar  --add-reads jdk.internal.vm.compiler=jdk.internal.vm.ci  --add-modules jdk.internal.vm.ci --add-modules jdk.internal.vm.compiler --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=ALL-UNNAMED
+
+
+java  --upgrade-module-path=mycompiler.jar --add-modules jdk.internal.vm.ci --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=ALL-UNNAMED -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation  -XX:+LogVMOutput  -XX:-TieredCompilation    -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI  -XX:+UseJVMCICompiler -Djvmci.Compiler=org.hccp.compiler.MyCompilerFactory  -XX:+PrintCompilation -jar mycompiler.jar 
+
+
+#java    --upgrade-module-path=mycompiler.jar  --add-reads jdk.internal.vm.compiler=jdk.internal.vm.ci  --add-modules jdk.internal.vm.ci --add-modules jdk.internal.vm.compiler --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=ALL-UNNAMED  -XX:+UnlockDiagnosticVMOptions -XX:+LogCompilation  -XX:+LogVMOutput  -XX:-TieredCompilation    -XX:+UnlockExperimentalVMOptions -XX:+EnableJVMCI  -XX:+UseJVMCICompiler -Djvmci.Compiler=org.hccp.compiler.MyCompilerFactory  -XX:+PrintCompilation -jar mycompiler.jar 
 
 #    -XX:-UseJVMCINativeLibrary
 #  --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=org.hccp.compiler
--- src/org/hccp/compiler/module-info.java
+++ src/org/hccp/compiler/module-info.java
@@ -1,6 +1,5 @@
-module jdk.internal.vm.compiler {
-    requires jdk.internal.vm.ci;
-    opens org.hccp.compiler;
-    provides jdk.vm.ci.services.JVMCIServiceLocator with org.hccp.compiler.MyServiceLocator;
+module org.hccp.compiler {
 
+    requires jdk.internal.vm.ci;
+    // other module dependencies
 }
