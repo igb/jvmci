b5f8c9805532ddb8d989ce69eb12184967cdb8ae
Ian Brown <ibrown@netflix.com>
2024-08-10 15:44:05 -0700

    working?

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf dist
 rm mycompiler.jar
 mkdir dist
+#javac	--release 9 --add-modules jdk.internal.vm.ci  --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=ALL-UNNAMED,ALL-UNNAMED --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=ALL-UNNAMED src/org/hccp/compiler/*.java  src/org/hccp/jvmci/test/*.java -d dist
 
 javac --add-modules jdk.internal.vm.ci  --add-exports jdk.internal.vm.ci/jdk.vm.ci.runtime=org.hccp.compiler --add-exports jdk.internal.vm.ci/jdk.vm.ci.code=org.hccp.compiler  --add-exports jdk.internal.vm.ci/jdk.vm.ci.services=org.hccp.compiler  src/org/hccp/compiler/*.java  src/org/hccp/jvmci/test/*.java -d dist
 
