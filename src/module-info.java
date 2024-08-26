ca7d1c7a01ab42735d3c3a26e8b279eda95b9874
Ian Brown <ibrown@netflix.com>
2024-08-26 12:51:56 -0700

    holy shit it works!

--- src/module-info.java
+++ src/module-info.java
@@ -4,8 +4,9 @@ module jdk.internal.vm.compiler {
     requires jdk.internal.vm.ci;
     requires jdk.management;
     requires jdk.unsupported;
-
+    opens org.hccp.compiler;
     exports org.hccp.compiler;
 
     provides jdk.vm.ci.services.JVMCIServiceLocator with org.hccp.compiler.MyServiceLocator;
+  
 }
